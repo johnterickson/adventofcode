afb7d2af91cc7a37c391b1c4acda6f3236b5b368
John Erickson <john.t.erickson@gmail.com>
2019-12-16 18:23:40 -0800

    day 8 part 1

--- 2019/src/day8.rs
+++ 2019/src/day8.rs
@@ -27,9 +27,63 @@ fn part1(input: &[usize]) -> usize {
     ones * twos
 }
 
+#[derive(Copy, Clone, PartialEq)]
+enum Color {
+    Black,
+    White,
+    Transparent,
+}
+
+impl Color {
+    fn parse(c: &usize) -> Color {
+        match c {
+            0 => Color::Black,
+            1 => Color::White,
+            2 => Color::Transparent,
+            _ => panic!(),
+        }
+    }
+
+    fn merge(&mut self, c: Color) {
+        if Color::Transparent == *self {
+            *self = c;
+        }
+    }
+}
+
+impl std::fmt::Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "{}", match self {
+            Color::Black => " ",
+            Color::White => "O",
+            Color::Transparent => "?",
+        })
+    }
+}
+
 #[aoc(day8, part2)]
 fn part2(input: &[usize]) -> usize {
-    unimplemented!();
+    let width = 25;
+    let height = 6;
+    let layers = input.len() / (width * height);
+    assert_eq!(input.len(), width*height*layers);
+    let layers = input.chunks(width*height);
+
+    let mut picture = vec![Color::Transparent; width*height];
+    for layer in layers {
+        for (i, pixel) in layer.iter().enumerate() {
+            picture[i].merge(Color::parse(pixel));
+        }
+    }
+
+    for h in 0..height {
+        for w in 0..width {
+            print!("{}", picture[h*width + w]);
+        }
+        println!();
+    }
+
+    0
 }
 
 #[cfg(test)]
