a66cba5f60b4def6aa77feb2b07aed940f00169d
John Erickson <johnterickson@github.com>
2021-12-18 00:11:46 -0800

    day4pt1

--- 2021/src/day4.rs
+++ 2021/src/day4.rs
@@ -10,7 +10,7 @@ fn parse_input(input: &str) -> (Vec<usize>, Vec<[[usize;5]; 5]>) {
     assert_eq!("", lines.next().unwrap().trim());
     let lines: Vec<_> = lines.collect();
     (
-        draws, 
+        draws,
         lines.chunks(6).map(|b| {
             let mut board = [[0;5]; 5];
             for (y, line) in b.iter().take(5).enumerate() {
@@ -25,7 +25,7 @@ fn parse_input(input: &str) -> (Vec<usize>, Vec<[[usize;5]; 5]>) {
 }
 
 #[aoc(day4, part1)]
-fn part1(inputs: &(Vec<usize>, Vec<[[usize;5]; 5]>)) -> u32 {
+fn part1(inputs: &(Vec<usize>, Vec<[[usize;5]; 5]>)) -> usize {
     let (draws, boards) = inputs;
 
     let mut boards: Vec<_> = boards.iter().map(|b| {
@@ -43,29 +43,40 @@ fn part1(inputs: &(Vec<usize>, Vec<[[usize;5]; 5]>)) -> u32 {
         [0b00100, 0b00100, 0b00100, 0b00100, 0b00100,],
         [0b00010, 0b00010, 0b00010, 0b00010, 0b00010,],
         [0b00001, 0b00001, 0b00001, 0b00001, 0b00001,],
-        [0b10000, 0b01000, 0b00100, 0b00010, 0b00001,],
-        [0b00001, 0b00010, 0b00100, 0b01000, 0b10000,],
+        // [0b10000, 0b01000, 0b00100, 0b00010, 0b00001,],
+        // [0b00001, 0b00010, 0b00100, 0b01000, 0b10000,],
     ];
 
     let is_winner = |hits: &[u8; 5]| {
         for win_mask in win_masks {
             if hits.iter().zip(win_mask).all(|(h, m)| h & m == m) {
-                dbg!(win_mask);
+                // dbg!(hits);
+                // dbg!(win_mask);
                 return true;
             }
         }
         false
     };
 
-    'winner :for d in draws {
+    for d in draws {
+        // dbg!(&d);
         for (board,hits) in boards.iter_mut() {
             for (y,row) in board.iter().enumerate() {
                 for (x,n) in row.iter().enumerate() {
                     if n == d {
                         hits[y] |= 1 << x;
                         if is_winner(hits) {
-                            dbg!(&board);
-                            break 'winner;
+                            let mut sum_unmarked = 0;
+                            // dbg!(&board);
+                            for yy in 0..5 {
+                                for xx in 0..5 {
+                                    if (hits[yy] >> xx) & 0x1 == 0 {
+                                        sum_unmarked += board[yy][xx];
+                                    }
+                                }
+                            }
+                            return d * sum_unmarked;
+
                         }
                     }
                 }
@@ -73,7 +84,7 @@ fn part1(inputs: &(Vec<usize>, Vec<[[usize;5]; 5]>)) -> u32 {
         }
     }
 
-    todo!();
+    unreachable!();
 }
 
 
@@ -83,7 +94,7 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        let input = 
+        let input =
     r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
     22 13 17 11  0
@@ -91,19 +102,19 @@ mod tests {
     21  9 14 16  7
      6 10  3 18  5
      1 12 20 15 19
-    
+
      3 15  0  2 22
      9 18 13 17  5
     19  8  7 25 23
     20 11 10 24  4
     14 21 16 12  6
-    
+
     14 21 17 24  4
     10 16 15  9 19
     18  8 23 26 20
     22 11 13  6  5
      2  0 12  3  7"#;
         let input = parse_input(input);
-        assert_eq!(part1(&input), 198);
+        assert_eq!(part1(&input), 4512);
     }
 }
