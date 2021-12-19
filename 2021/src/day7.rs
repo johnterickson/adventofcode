191cea5d44d44ad2f16db90ca077c7e55e2f5dcf
John Erickson <johnterickson@github.com>
2021-12-19 08:08:11 -0800

    day 7

--- 2021/src/day7.rs
+++ 2021/src/day7.rs
@@ -1,3 +1,5 @@
+use std::ops::{RangeInclusive};
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day7)]
@@ -16,9 +18,34 @@ fn part1(positions: &Vec<i64>) -> i64 {
     positions.iter().map(|p| (p-target).abs()).sum()
 }
 
+fn sum_of_integers(r:RangeInclusive<i64>) -> i64 {
+    (r.end() - r.start() + 1)*(r.start() + r.end())/2
+}
+
+fn fuel(a: i64, b:i64) -> i64 {
+    if a > b {
+        fuel(b, a)
+    } else {
+        assert!(b>=a);
+        sum_of_integers(0..=(b-a))
+    }
+}
+
 #[aoc(day7, part2)]
 fn part2(positions: &Vec<i64>) -> i64 {
-    todo!();
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    let mut min_fuel = i64::MAX;
+    for i in min..=max {
+        let fuel: i64 = positions.iter().map(|p| fuel(i,*p)).sum();
+        // dbg!(i,&fuel);
+        if fuel < min_fuel {
+            min_fuel = fuel;
+        }
+    }
+
+    min_fuel
 }
 
 #[cfg(test)]
@@ -26,9 +53,25 @@ mod tests {
     use super::*;
 
     #[test]
+    fn helpers() {
+        assert_eq!(sum_of_integers(1..=3), 1 + 2 + 3); 
+        assert_eq!(sum_of_integers(1..=4), 1 + 2 + 3 + 4); 
+        assert_eq!(sum_of_integers(2..=4), 2 + 3 + 4); 
+        assert_eq!(fuel(5,16), 66); 
+        assert_eq!(fuel(5,1), 10); 
+    }
+
+    #[test]
     fn part1_example() {
         let input = r#"16,1,2,0,4,2,7,1,2,14"#;
         let input = parse_input(input);
         assert_eq!(part1(&input), 37);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = r#"16,1,2,0,4,2,7,1,2,14"#;
+        let input = parse_input(input);
+        assert_eq!(part2(&input), 168);
     }
 }
