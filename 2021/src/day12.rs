f59644d8fdb8abfa6d9ea05a0ce638f36fb6c8e6
John T Erickson <johnterickson@github.com>
2021-12-22 15:51:36 -0800

    day 12

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -3,7 +3,7 @@ use std::collections::{BTreeMap, BTreeSet};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum CaveName {
     Start,
     End,
@@ -82,6 +82,51 @@ fn part1(caves: &BTreeMap<CaveName, BTreeSet<CaveName>>) -> usize {
     paths.len()
 }
 
+#[aoc(day12, part2)]
+fn part2(caves: &BTreeMap<CaveName, BTreeSet<CaveName>>) -> usize { 
+
+    let mut paths = BTreeSet::new();
+    let mut path = Vec::new();
+
+    fn dfs<'a>(caves: &'a BTreeMap<CaveName, BTreeSet<CaveName>>, this: &'a CaveName, can_visit_twice: Option<&'a CaveName>, path: &mut Vec<&'a CaveName>, complete_paths: &mut BTreeSet<Vec<&'a CaveName>>) {
+        let mut this_is_second_visit = false;
+
+        if path.contains(&this) {
+            if Some(this) == can_visit_twice {
+                this_is_second_visit = true;
+            } else if !this.can_revisit() {
+                return;
+            }
+        }
+
+        path.push(this);
+        // println!("{:?} {:?}", can_visit_twice, &path);
+
+        if this == &CaveName::End {
+            complete_paths.insert(path.clone());
+        } else {
+            for n in caves[&this].iter() {
+                let can_visit_twice = if this_is_second_visit { None } else { can_visit_twice };
+                dfs(caves, n, can_visit_twice, path, complete_paths);
+            }
+        }
+        
+        path.pop();
+    }
+
+    for cave in caves.keys() {
+        match cave {
+            CaveName::Small(_) => {
+                dfs(caves, &CaveName::Start, Some(cave), &mut path, &mut paths);
+                assert_eq!(path.len(), 0);
+            }
+            _ => {}
+        }
+    }
+
+    paths.len()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -137,5 +182,58 @@ zg-he
 pj-fs
 start-RW"#);
        assert_eq!(part1(&input), 226);
+    }
+
+    #[test]
+    fn part2_example() {
+       let input = parse_input(
+r#"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"#);
+       assert_eq!(part2(&input), 36);
+    }
+
+    #[test]
+    fn part2_example2() {
+       let input = parse_input(
+r#"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"#);
+       assert_eq!(part2(&input), 103);
+    }
+
+    #[test]
+    fn part2_example3() {
+       let input = parse_input(
+r#"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"#);
+       assert_eq!(part2(&input), 3509);
     }
 }
