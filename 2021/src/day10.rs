2d362ffa0b1989335b7e4717d45a03eb8dbe09bb
John Erickson <johnterickson@github.com>
2021-12-21 18:47:20 -0800

    day 10

--- 2021/src/day10.rs
+++ 2021/src/day10.rs
@@ -45,6 +45,57 @@ fn part1(lines: &Vec<String>) -> u64 {
     score
 }
 
+
+#[aoc(day10, part2)]
+fn part2(lines: &Vec<String>) -> u64 { 
+    
+    let mut matching = BTreeMap::new();
+    matching.insert(')','(');
+    matching.insert(']','[');
+    matching.insert('}','{');
+    matching.insert('>','<');
+
+    let mut scores = Vec::new();
+    
+    for line in lines {
+        let mut tokens = VecDeque::new();
+        let mut valid = true;
+        for c in line.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => tokens.push_back(c),
+                c => {
+                    let expect = Some(matching[&c]);
+                    let actual = tokens.pop_back();
+                    if expect != actual {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if valid {
+            let mut score = 0;
+
+            for c in tokens.iter().rev() {
+                score *= 5;
+                score += match c {
+                    '(' => 1,
+                    '[' => 2,
+                    '{' => 3,
+                    '<' => 4,
+                    _ => panic!(),
+                };
+            }
+
+            scores.push(score);
+        }
+    }
+
+    scores.sort();
+    scores[scores.len()/2]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,5 +114,21 @@ r#"[({(<(())[]>[[{[]{<()<>>
     <{([([[(<>()){}]>(<<{{
     <{([{{}}[<[[[<>{}]]]>[]]"#);
        assert_eq!(part1(&input), 26397);
+    }
+
+    #[test]
+    fn part2_example() {
+       let input = parse_input(
+r#"[({(<(())[]>[[{[]{<()<>>
+    [(()[<>])]({[<{<<[]>>(
+    {([(<{}[<>[]}>{[]{[(<()>
+    (((({<>}<{<{<>}{[]{[]{}
+    [[<[([]))<([[{}[[()]]]
+    [{[{({}]{}}([{[{{{}}([]
+    {<[[]]>}<{[{[{[]{()[[[]
+    [<(<(<(<{}))><([]([]()
+    <{([([[(<>()){}]>(<<{{
+    <{([{{}}[<[[[<>{}]]]>[]]"#);
+       assert_eq!(part2(&input), 288957);
     }
 }
