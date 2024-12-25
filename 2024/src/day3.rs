6a6b95931e80b71f54992871b3eb609f2696f6c3
John Erickson <john.t.erickson@gmail.com>
2024-12-25 10:02:13 -0800

    add 2024 to CI

---
+++ 2024/src/day3.rs
@@ -0,0 +1,158 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use nom::{branch::alt, bytes::complete::{tag, take_while_m_n}, combinator::{map, map_res}, sequence::tuple, IResult};
+
+/*
+
+--- Day 3: Mull It Over ---
+"Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the North Pole Toboggan Rental Shop. The Historians head out to take a look.
+
+The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"
+
+The computer appears to be trying to run a program, but its memory (your puzzle input) is corrupted. All of the instructions have been jumbled up!
+
+It seems like the goal of the program is just to multiply some numbers. It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.
+
+However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
+
+For example, consider the following section of corrupted memory:
+
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
+
+Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+
+ */
+
+#[derive(Debug)]
+struct Mul {
+    x: u32,
+    y: u32,
+}
+
+fn number(input: &str) -> IResult<&str, u32> {
+    use std::str::FromStr;
+    map_res(take_while_m_n(1, 3, |c: char| c.is_digit(10)), u32::from_str)(input)
+}
+
+fn parse_mul(input: &str) -> IResult<&str, Mul> {
+    map(
+        tuple((tag("mul("), number, tag(","), number, tag(")"))),
+        |(_, x, _, y, _)| Mul { x, y },
+    )(input)
+}
+
+#[aoc_generator(day3)]
+fn parse_input(input: &str) -> Vec<Instruction> {
+    let input = input.trim();
+    let input = input.as_bytes();
+    input
+        .iter()
+        .enumerate()
+        .filter_map(|(i, _c)| {
+            let s = std::str::from_utf8(&input[i..]).unwrap();
+            if let Ok((_str, inst)) = parse_inst(s) {
+                // dbg!("{} -> {:?}", s, &inst);
+                Some(inst)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+#[aoc(day3, part1)]
+fn part1(ops: &Vec<Instruction>) -> u32 {
+    ops.iter()
+    .filter_map(|op| if let Instruction::Mul(mul) = op {
+        Some(mul.x * mul.y)
+    } else {
+        None
+    }).sum()
+}
+
+/*
+--- Part Two ---
+As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.
+
+There are two new instructions you'll need to handle:
+
+The do() instruction enables future mul instructions.
+The don't() instruction disables future mul instructions.
+Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+For example:
+
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+
+Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+*/
+
+#[derive(Debug)]
+enum Instruction {
+    Mul(Mul),
+    Do,
+    Dont,
+}
+
+fn parse_inst(input: &str) -> IResult<&str, Instruction> {
+    alt((
+        map(parse_mul, |m| Instruction::Mul(m)),
+        map(tag("do()"), |_| Instruction::Do),
+        map(tag("don't()"), |_| Instruction::Dont),
+    ))(input)
+}
+
+#[aoc(day3, part2)]
+fn part2(ops: &Vec<Instruction>) -> u32 {
+    let mut enabled = true;
+    ops.iter()
+        .filter_map(|op| match op {
+            Instruction::Mul(m) => {
+                if enabled {
+                    Some(m)
+                } else {
+                    None
+                }
+            }
+            Instruction::Do => {
+                enabled = true;
+                None
+            }
+            Instruction::Dont => {
+                enabled = false;
+                None
+            }
+        })
+        .map(|op| op.x * op.y)
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let input = parse_input(
+            r#"
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        "#
+            .trim(),
+        );
+        assert_eq!(part1(&input), 161);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = parse_input(
+            r#"
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        "#
+            .trim(),
+        );
+        assert_eq!(part2(&input), 48);
+    }
+}
